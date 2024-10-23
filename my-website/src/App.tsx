78fc96c5a481bf2c72a2d1e6f97031cb721a5560
tugaserya <artempoznakov990@gmail.com>
2024-10-23 10:22:57 +0300

    first commit

---
+++ my-website/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Header from './components/Header'
+
+const App = () => {
+
+  return (
+   <Header />
+  )
+}
+
+export default App
---
+++ my-website/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import './Header.css'
+import './Button'
+import Button from './Button'
+
+export default function Header() {
+return (
+    <header>
+        <h1>Kilka_v_HJIebe's website</h1>
+        <Button icon="nf nf-md-link" title=" Social links"/>
+        <Button icon="nf nf-oct-project_roadmap" title=" Projects"/>
+        <Button icon="nf nf-cod-github" title=" Github"/>
+    </header>
+)
+}
