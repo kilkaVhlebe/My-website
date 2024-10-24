7eae824c49ae305b5e57d08297e44db815981edc
tugaserya <artempoznakov990@gmail.com>
2024-10-24 19:51:50 +0300

    add new style and change varielblse

--- my-website/src/components/Button.tsx
+++ my-website/src/components/Button.tsx
@@ -1,6 +1,6 @@
 import './Button.css'
 
-export default function Button(props: {title: string, icon: string}) {
+export default function Button(props: {title?: string, icon: string}) {
     return (
         <button>
             <span className={props.icon}></span><span>{props.title}</span>
