f6be448d3191732374b7980746316e4131aed3ba
tugaserya <artempoznakov990@gmail.com>
2024-10-24 19:53:10 +0300

    add ASCII ART

--- my-website/src/App.tsx
+++ my-website/src/App.tsx
@@ -1,10 +1,26 @@
 import './App.css'
 import Header from './components/Header'
 
+const Art ="                                           #\n                                      ###\n                            #############                                     ###\n                  #######################              #####\n        #### . ###############################\n#######   ##############################                                         Yes - it's literally me\n#######################################\n      #(     )########################              ####\n          #########################\n               ###################\n                           ###            ###\n                                #                 #"
+
+function TextArt (props: {label: string, text: string}) {
+  return (
+    <pre
+      aria-label={props.label}
+      className="text-art"
+    >{props.text}</pre>
+  );
+}
+
+
 const App = () => {
 
   return (
+   <>
    <Header />
+   <TextArt label="literly me" text={Art}/>
+   <span></span>
+   </>
   )
 }
 
