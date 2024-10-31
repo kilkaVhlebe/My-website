7abd3dc2e77fe9ae6fc0975d5fa4620e8dbb38e2
Kilka_v_HJlebe <artempoznakov990@gmail.com>
2024-10-31 18:30:53 +0300

    change the asciiart on main page to screen

--- my-website/src/pages/MainPage.tsx
+++ my-website/src/pages/MainPage.tsx
@@ -1,21 +1,9 @@
 
 
-const Art ="                                           #\n                                      ###\n                            #############                                     ###\n                  #######################              #####\n        #### . ###############################\n#######   ##############################                                         Yes - it's literally me\n#######################################\n      #(     )########################              ####\n          #########################\n               ###################\n                           ###            ###\n                                #                 #"
-function TextArt (props: {label: string, text: string}) {
-  return (
-    <pre
-    
-      aria-label={props.label}
-      className="text-art"
-    >{props.text}
-    </pre>
-  );
-}
-
 const MainPage = () => {
     return (
       <div>
-        <TextArt label="literly me" text={Art}/>
+        <img src="../../public/ascii-art.png"/>
       </div>
     );
   }
