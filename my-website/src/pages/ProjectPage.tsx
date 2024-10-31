75af3f039f5d379d547129f420510d36a2d1a56b
Kilka_v_HJlebe <artempoznakov990@gmail.com>
2024-10-31 18:47:50 +0300

    ABobA

--- my-website/src/pages/ProjectPage.tsx
+++ my-website/src/pages/ProjectPage.tsx
@@ -3,12 +3,11 @@ import ProjectItem from "../components/ProjectItem"
 export default function ProjectPage() {
     return (
         <div>
-              <ProjectItem name="Name" description="description" link="https://killmyself.ru/" /> 
-              <ProjectItem name="Name" description="description" link="https://killmyself.ru/" />  
-              <ProjectItem name="Name" description="description" link="https://killmyself.ru/" />  
-              <ProjectItem name="Name" description="description" link="https://killmyself.ru/" />  
-              <ProjectItem name="Name" description="description" link="https://killmyself.ru/" />  
-              
+            <h3>Click on proj to redirect</h3>
+            <p>Here's only released projects. I have more in process or non released but done projects on my github!</p>
+              <ProjectItem name="ASCII-ART app" description="Andriod app for generation ascii images from real photo" link="https://github.com/kilkaVhlebe/ASCII-cam" /> 
+              <ProjectItem name="Circle-Drawer script" description="Js script for drawing circles on perfect-circle.io" link="https://github.com/kilkaVhlebe/Circle-Drawer" />  
+              <ProjectItem name="Step-by-Dungeon" description="Turn based game maked on godot. 2 minutes gameplay lol." link="https://github.com/kilkaVhlebe/Step-by-Dungeon.git" /> 
         </div>
     )
 }
--- my-website/src/pages/SocialsPage.tsx
+++ my-website/src/pages/SocialsPage.tsx
@@ -9,6 +9,7 @@ export default function ProjectPage() {
             <SocialItem name="Telegram" imgLink="../../public/telegram-svgrepo-com.png" link="https://t.me/Kilkavhlebe"/>
             <SocialItem name="YouTube" imgLink="../../public/youtube-svgrepo-com.png" link="https://www.youtube.com/@kilka_v_hJlebe"/>
             <SocialItem name="Twitch" imgLink="../../public/logo-twitch-svgrepo-com.png" link="https://www.twitch.tv/kilka_v_hjiebe"/>
+            <SocialItem name="GitHub" imgLink="../../public/github-icon-1-logo-svgrepo-com.png" link="https://github.com/kilkaVhlebe"/>
         </div>
     )
 }
