f7c74bad72a2a8e7443023b29fcf2a1a1333179c
Kilka_v_HJlebe <artempoznakov990@gmail.com>
2024-10-31 18:47:40 +0300

    modyfied html structure

--- my-website/src/components/ProjectItem.tsx
+++ my-website/src/components/ProjectItem.tsx
@@ -3,9 +3,11 @@ import './ProjectItem.css'
 export default function ProjectItem(props: {name:string, description: string, link: string, imgLink?: string, width?: number, height?: number}) {
  return (
     <div className="ProjectItem">
-        <h3><a href={props.link}>{props.name}</a></h3>
+        <a href={props.link}>
+        <h3>{props.name}</h3>
         <p>{props.description}</p>
         <img src={props.imgLink} width={props.width}height={props.height}/>
+        </a>
     </div>
  )
 }
