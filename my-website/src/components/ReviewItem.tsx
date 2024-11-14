825b17d30d858d36e2f10c370ffc1c0484ce8fdb
Kilka_v_HJIebe <artempoznakov990@gmail.com>
2024-11-14 10:29:45 +0300

    change rewiev item styles and change span to div

--- my-website/src/components/ReviewItem.tsx
+++ my-website/src/components/ReviewItem.tsx
@@ -1,9 +1,14 @@
-
+import './ReviewItem.css'
 
 export default function ReviewItem() {
     return (
-        <div>
-            
-        </div>
+        <button className='item'>
+            <div className='item_box'>
+            <div>ahuenno</div>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_xbpQMf73dtCr99cLyBOYYaHuIKGTCzmjOA&s" height={150} width={200}>
+            </img>
+            <p>aboba</p>
+            </div>
+        </button>
     )
 }
