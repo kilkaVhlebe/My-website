c4793603242ff641f4522c56cebbd68ccb272e92
Kilka_v_HJIebe <artempoznakov990@gmail.com>
2024-11-14 10:30:37 +0300

    change game page

--- my-website/src/pages/GameReviews.tsx
+++ my-website/src/pages/GameReviews.tsx
@@ -1,7 +1,27 @@
+import ReviewItem from "../components/ReviewItem";
+import  './GameReviews.css'
+
 export default function GameReviews() {
     return (
         <div>
-            
+            <div className="filter_bar">
+                <input className="filter_input"></input>
+                <button className="filter_button">Filters</button>
+            </div>
+            <div className="review_list">
+                <ReviewItem />
+                <ReviewItem />
+                <ReviewItem />
+                <ReviewItem />
+                <ReviewItem />
+                <ReviewItem />
+                <ReviewItem />
+                <ReviewItem />
+                <ReviewItem />
+                <ReviewItem />
+                <ReviewItem />
+                <ReviewItem />
+            </div>
         </div>
     )
 }
